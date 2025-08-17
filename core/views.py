6d0841d2c7fb665f39bc9da935c3d009e8055978
Alexey <bogwowwww@mail.ru>
2025-08-17 12:00:12 +0300

    hw42: добавлен prefetch related и select related для детального отображения заявки

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from .data import orders
 from .models import Order, Master, Service
 from .forms import OrderForm
-from django.db.models import Q, Count
+from django.db.models import Q, Count, Sum
 
 
 def landing(request):
@@ -107,18 +107,11 @@ def order_detail(request, order_id):
     :param request: HttpRequest
     :param order_id: int (номер заказа)
     """
-    order = [order for order in orders if order["id"] == order_id]
-    try:
-        order = order[0]
-        context = {
-            "order": order,
-            "my_fariable": "Hello, world!",
-        }
-    except IndexError:
-        return HttpResponse("<h1>Заказ не найден</h1>", status=404)
+    order = Order.objects.prefetch_related("services").select_related("master").get(id=order_id)
 
-    else:
-        return render(request, "order_detail.html", context=context)
+    context = {"order": order}
+
+    return render(request, "order_detail.html", context=context)
 
 
 def order_page(request):
